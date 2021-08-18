63172d07edc0182a2ce0d0c560d664a55f52ffa6
Hisashi Itoh <it.hisas@gmail.com>
2021-08-18 15:34:02 +0900

    update

--- app.py
+++ app.py
@@ -7,6 +7,8 @@
 #       FastAPI のレッスン
 #
 from fastapi import FastAPI
+from typing import List, Dict
+from typing import Optional
 
 app = FastAPI()
 
@@ -14,3 +16,10 @@ app = FastAPI()
 @app.get('/')
 async def hello():
     return {"text": "hello world!"}
+
+@app.get('/get/{path}')
+async def path_and_query_params(
+        path: str,
+        query: int,
+        default_none: Optional[str] = None): # optional で型
+    return {"text": f"hello, {path}, {query} and {default_none}"}
